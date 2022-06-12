3903ef7e674344b1c3cedd76ae6091dedb3c8222
Stebbi <stefodd@yahoo.com>
2022-06-12 16:51:10 -0500

    insomnia 404 troubleshooting

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    addThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thought-controller');
+
+router
+    .route('/')
+    .get(getAllThoughts);
+
+router
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router
+    .route('/:userId')
+    .post(addThought);
+
+router
+    .route('/:thoughtId/reactions')
+    .post(addReaction)
+    .delete(deleteReaction)
+
+module.exports = router;
