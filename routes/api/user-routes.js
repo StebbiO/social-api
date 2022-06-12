094f7c9196cc9ef5bce7a799f865be9b3a8ce74d
Stebbi <stefodd@yahoo.com>
2022-06-12 16:20:18 -0500

    controllers + user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
+const { create } = require('../../models/User');
+
+router
+    .route('/')
+    .get()
+    .post();
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+router
+    .route('/:id/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);
+
+module.exports = router;
